1455fadc65002c5a742b9c757cd979f273e265c5
harshu5797 <a1763100@student.adelaide.edu.au>
2019-10-04 18:19:12 +0930

    commited

--- eVotingApp/authentication/views.py
+++ eVotingApp/authentication/views.py
@@ -59,13 +59,12 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         user = authenticate(username=username, password=password)
 
         if user:
             if user.is_active:
                 login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse('home'))
 
 
             else:
