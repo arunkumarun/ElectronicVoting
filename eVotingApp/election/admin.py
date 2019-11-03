1754b697193a89797086e8a71be94aebdce12d79
Subu17 <subu17may@gmail.com>
2019-11-03 12:52:28 +1030

    Commented Election

--- eVotingApp/election/admin.py
+++ eVotingApp/election/admin.py
@@ -1,7 +1,12 @@
+# Django Admin functionality
+
 from django.contrib import admin
 from election.models import Party,Candidate,ElectionDetails
 # Register your models here.
 
+# Register Party information to admin page
 admin.site.register(Party)
+# Register Candidate information to admin page
 admin.site.register(Candidate)
+# Register the election details to admin page
 admin.site.register(ElectionDetails)
